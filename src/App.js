4fb7cd289c5fb6005a3659b9ea66c80e27598b37
rebelBeard <rfm144@gmail.com>
2017-02-28 23:01:51 -0500

    added toggling functionality to remove the editor, and get a full view of your markdown without scrolling.

--- src/App.js
+++ src/App.js
@@ -5,25 +5,36 @@ let marked = require('marked');
 let App = React.createClass ({
 
   getInitialState: function() {
-    return {output: ''}
+    return {output: '',
+            showHideEditor: 'show'
+          }
   },
 
   update: function() {
     this.setState({output: this.refs.text.value})
   },
 
+  toggleEditor: function() {
+    if (this.state.showHideEditor === 'show') {
+      this.setState({showHideEditor: 'hidden'});
+      this.refs.view.style.width = '95vw';
+    } else {
+      this.setState({showHideEditor: 'show'});
+      this.refs.view.style.width = '45vw';
+    }
+  },
+
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
+        <button onClick={this.toggleEditor}>Toggle Editor</button>
           <h2>Markdown Previewer</h2>
-          <div>
-            <span className="right-arrow hidden">Switch View →</span>
-          </div>
         </div>
         <div className="row">
-          <textarea onInput={this.update} ref='text'></textarea>
-          <div className="view" dangerouslySetInnerHTML={{__html: marked(this.state.output)}}></div>
+          <textarea onInput={this.update} ref='text' className={this.state.showHideEditor}></textarea>
+          <div className='view' ref='view' dangerouslySetInnerHTML={{__html: marked(this.state.output)}}></div>
         </div>
       </div>
     );
